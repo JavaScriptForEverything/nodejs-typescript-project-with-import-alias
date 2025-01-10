03e9920cb57b0d763f81e0e5248711fda16683d1
Riajul Islam <javascriptForEverything@gmail.com>
2025-01-10 19:04:15 +0600

    use without src so that import alias also work on test files too

--- __tests__/product.test.ts
+++ __tests__/product.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import app from '../src/app'
+import app from '@/app'
 
 it('GET /api/products', async () => {
 	const res = await request(app)
--- jest.config.ts
+++ jest.config.ts
@@ -2,15 +2,14 @@ import type { Config } from 'jest'
 
 const config: Config = {
 	moduleNameMapper: {
-		// '^@/(.*)$': '<rootDir>/$1'
-		'^@/(.*)$': '<rootDir>/src/$1'
+		'^@/(.*)$': '<rootDir>/$1'
 	},
 
   preset: 'ts-jest',
   testEnvironment: "jest-environment-node",
 	verbose: true,
 	testMatch: ["**/__tests__/**/*.test.ts"],
-	// setupFilesAfterEnv: ['./src/jest.setup.ts'],
+	setupFilesAfterEnv: ['./jest.setup.ts'],
 }
 
 export default config
