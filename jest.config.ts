d2ab5cd8102ea696358cabd82c4618fe6944baea
Riajul Islam <javascriptForEverything@gmail.com>
2025-01-31 18:05:29 +0600

    setup import alias in src directory

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,20 @@
 import type { Config } from 'jest'
 
 const config: Config = {
+  preset: 'ts-jest', 						// to use ts-jest instead of jest
+  testEnvironment: "node", 			// for nodejs language
+  roots: ['<rootDir>/src'], 		// Set the root directory to 'src'
+
 	moduleNameMapper: {
-		'^@/(.*)$': '<rootDir>/$1'
+		'^@/(.*)$': '<rootDir>/src/$1'
 	},
 
-  preset: 'ts-jest',
-  testEnvironment: "jest-environment-node",
+  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }), // Map import aliases
+  // transform: {
+  //   '^.+\\.ts$': 'ts-jest',
+  // },
+  // moduleFileExtensions: ['ts', 'js', 'json'],
+
 	verbose: true,
 	testMatch: ["**/__tests__/**/*.test.ts"],
 	setupFilesAfterEnv: ['./jest.setup.ts'],
